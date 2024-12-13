b0241da3589014d0467938153f82d80bd033ae06
01Lusifer <01ruwantha@gmail.com>
2024-12-13 06:07:39 +0530

    create UserDetailRepository interface

---
+++ backend/shopx/src/main/java/com/comrepublic/shopx/auth/repositories/UserDetailRepository.java
@@ -0,0 +1,10 @@
+package com.comrepublic.shopx.auth.repositories;
+
+import com.comrepublic.shopx.auth.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDetailRepository extends JpaRepository<User,Long> {
+    User findByEmail(String username);
+}
