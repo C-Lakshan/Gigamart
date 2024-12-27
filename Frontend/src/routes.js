fe7137c04b02c4ae33dafa41ba6cfe0eae7ddcf4
01Lusifer <01ruwantha@gmail.com>
2024-12-27 15:59:29 +0530

    add routes to Login and Register pages

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -4,6 +4,9 @@ import ProductListPage from "./pages/ProductListPage/ProductListPage";
 import ShopApplicationWrapper from "./pages/ShopApplicationWrapper";
 import ProductDetails from "./pages/ProductDetailPage/ProductDetails";
 import { loadProductById } from "./routes/products";
+import AuthenticationWrapper from "./pages/AuthenticationWrapper";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -30,6 +33,20 @@ export const router = createBrowserRouter([
           path:"/product/:productId",
           loader: loadProductById,
           element:<ProductDetails />,
+        }
+      ]
+    },
+    {
+      path:"/v1/",
+      element:<AuthenticationWrapper/>,
+      children:[
+        {
+          path:"login",
+          element:<Login />
+        },
+        {
+          path:"register",
+          element:<Register />
         }
       ]
     }
