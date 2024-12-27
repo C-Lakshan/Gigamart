babe3b62e9d3b3f974ee0d62f9b565ec63b60da6
01Lusifer <01ruwantha@gmail.com>
2024-12-27 16:01:46 +0530

    add some changes

--- Frontend/src/components/Navigation/Navigation.jsx
+++ Frontend/src/components/Navigation/Navigation.jsx
@@ -6,7 +6,7 @@ import './Navigation.css';
 import { Link, NavLink} from 'react-router-dom'
 
 
-const Navigation = () => {
+const Navigation = ({variant="default"}) => {
   return (
    <nav className='flex items-center py-6 px-8 justify-between gap-40'>
     <div className='flex items-center gap-6'>
@@ -14,6 +14,7 @@ const Navigation = () => {
         <a className='text-3xl text-black gap-8' href='/'>GigaMart</a>
 
     </div>
+    { variant ==="default" &&
     <div className='flex flex-wrap items-center font-bold gap-8 flex-1'>
         {/* Nav items */}
         <ul className='flex gap-14 text-gray-600 hover:text-black'>
@@ -23,6 +24,8 @@ const Navigation = () => {
             <li><NavLink to='/Accessories' className={({isActive})=> isActive ? 'active-link':''}>Accessories</NavLink></li>
         </ul>
     </div>
+    }
+    { variant ==="default" &&
     <div className='flex justify-center'>
         {/* Search Bar */}
         <div className='border rounded flex overflow-hidden'>
@@ -34,15 +37,23 @@ const Navigation = () => {
         
         </div>
     </div>
-    
+    }
+
     <div className='flex flex-wrap items-center gap-4'>
         {/* Action Items - icons */}
+        { variant ==="default" &&
         <ul className='flex items-center gap-8'>
             <li><button><Wishlist /></button></li>
             <li><button><AccountIcon /></button></li>
             <li><Link to='/cart-irems'><CartIcon /></Link></li>
-        </ul>
-
+        </ul>}
+        {
+            variant ==="auth" &&
+            <ul className='flex gap-8'>
+                <li className='text-black border border-black hover:bg-slate-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'><NavLink to="/v1/login" className={({isActive})=> isActive ? 'active-link':''}>Login</NavLink></li>
+                <li className='text-black border border-black hover:bg-slate-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'><NavLink to="/v1/register" className={({isActive})=> isActive ? 'active-link':''}>Signup</NavLink></li>
+            </ul>
+        }
     </div>
    </nav>
   )
