163412ae2d3f80a1832f22a9fc311b0aa8c04a24
C-Lakshan <chathurangalakshan35@gmail.com>
2025-01-01 14:30:25 +0530

    correct error

--- backend/shopx/src/main/java/com/comrepublic/shopx/auth/repositories/UserDetailRepository.java
+++ backend/shopx/src/main/java/com/comrepublic/shopx/auth/repositories/UserDetailRepository.java
@@ -3,8 +3,9 @@ package com.comrepublic.shopx.auth.repositories;
 import com.comrepublic.shopx.auth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
 @Repository
-public interface UserDetailRepository extends JpaRepository<User,Long> {
+public interface UserDetailRepository extends JpaRepository<User,UUID> {
     User findByEmail(String username);
 }
