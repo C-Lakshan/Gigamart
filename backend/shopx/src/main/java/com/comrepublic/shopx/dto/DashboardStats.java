f62dcbb92fd2148d9880e084c4bf782e9cd37621
vihanga900 <vihangan20@gmail.com>
2025-02-09 07:11:57 +0530

    Added DTO for dashboard status

---
+++ backend/shopx/src/main/java/com/comrepublic/shopx/dto/DashboardStats.java
@@ -0,0 +1,19 @@
+package com.comrepublic.shopx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DashboardStats {
+    private long totalUsers;
+    private double totalSales;
+    private long totalCustomers;
+    private long totalOrders;
+    private long totalTransactions;
+}
+
